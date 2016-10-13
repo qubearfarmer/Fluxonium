c3094ff3324311ebad4179bd927498f637d216d3
nguyen89 <nbaolong89@gmail.com>
2016-10-13 12:07:06 -0400

    update script

--- Scripts/Sensitivity_wSquid.py
+++ Scripts/Sensitivity_wSquid.py
@@ -1,4 +1,4 @@
-from Fluxonium_hamiltonians.Squid_small_junction import bare_hamiltonian as H
+from Fluxonium_hamiltonians.Squid_small_junctions import bare_hamiltonian
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -23,17 +23,19 @@ A_c = 1.49982268962e-10
 beta_squid = 2.22e-3
 beta_ext = 0.341308382441
 d=9.9299e-2
-current = np.linspace(0,0.05,6)
-sensitivity = np.zeros(len(current))
+current = np.linspace(0.04,0.05,501)
 energies = np.zeros((len(current),level_num))
+energies_pExt = np.zeros((len(current),level_num))
+energies_pSquid = np.zeros((len(current),level_num))
 
 iState = 0
-fState = 1
-wr = 10.304
-g = 0.084
+fState = 2
+dPhi1 = 1.0e-3
+dPhi2 = 1.0e-3
 
 #######################################################################################################################
 #Simulation part
+# '''
 #Compute spectrum
 for idx, curr in enumerate(current):
     flux_squid = curr*B_coeff*A_j*1e-4
@@ -43,22 +45,47 @@ for idx, curr in enumerate(current):
     for idy in range(level_num):
         energies[idx,idy] = H.eigenenergies()[idy]
 
-np.savetxt(path+"_"+str(iState)+str(fState)+"_energies.txt", energies)
+for idx, curr in enumerate(current):
+    flux_squid = curr*B_coeff*A_j*1e-4
+    flux_ext = curr*B_coeff*A_c*1e-4
+    H = bare_hamiltonian(N, E_l, E_c, E_j_sum, d, 2*np.pi*(flux_squid/phi_o - beta_squid+dPhi1),
+                         2 * np.pi * (flux_ext / phi_o - beta_ext))
+    for idy in range(level_num):
+        energies_pSquid[idx,idy] = H.eigenenergies()[idy]
 
+for idx, curr in enumerate(current):
+    flux_squid = curr*B_coeff*A_j*1e-4
+    flux_ext = curr*B_coeff*A_c*1e-4
+    H = bare_hamiltonian(N, E_l, E_c, E_j_sum, d, 2*np.pi*(flux_squid/phi_o - beta_squid),
+                         2 * np.pi * (flux_ext / phi_o - beta_ext+dPhi2))
+    for idy in range(level_num):
+        energies_pExt[idx,idy] = H.eigenenergies()[idy]
+
+np.savetxt(path+"_"+str(iState)+str(fState)+"_energies.txt", energies)
+np.savetxt(path+"_"+str(iState)+str(fState)+"_energies_pSquid.txt", energies_pSquid)
+np.savetxt(path+"_"+str(iState)+str(fState)+"_energies_pExt.txt", energies_pExt)
+# '''
 #######################################################################################################################
 #Plotting part
 energies = np.genfromtxt(path+"_"+str(iState)+str(fState)+"_energies.txt")
-
+energies_pSquid = np.genfromtxt(path+"_"+str(iState)+str(fState)+"_energies_pSquid.txt")
+energies_pExt = np.genfromtxt(path+"_"+str(iState)+str(fState)+"_energies_pExt.txt")
 fig, ax1 = plt.subplots()
-ax1.plot(current*1e3,energies[:,fState]-energies[:,iState], color = 'k')
+trans_energy = energies[:,fState]-energies[:,iState]
+ax1.plot(current*1e3,trans_energy, color = 'k')
 ax1.set_ylabel('Transition energy')
 ax1.set_xlabel('Current (mA)')
 for tl in ax1.get_yticklabels():
     tl.set_color('k')
 
+trans_energy_pSquid=energies_pSquid[:,fState]-energies_pSquid[:,iState]
+trans_energy_pExt=energies_pExt[:,fState]-energies_pExt[:,iState]
+sensitivity_pSquid = abs(trans_energy_pSquid - trans_energy)/dPhi1
+sensitivity_pExt = abs(trans_energy_pExt - trans_energy)/dPhi2
+
 ax2 = ax1.twinx()
-ax2.plot(current*1e3, chi*1e3, 'b.')
-ax2.set_ylabel('Dispersive shift (MHz)')
+ax2.plot(current*1e3, sensitivity_pExt, 'b--',current*1e3, sensitivity_pSquid, 'r--')
+ax2.set_ylabel('Sensitivity')
 # ax2.set_ylim([-0.5,0.5])
 for t2 in ax2.get_yticklabels():
     t2.set_color('b')
