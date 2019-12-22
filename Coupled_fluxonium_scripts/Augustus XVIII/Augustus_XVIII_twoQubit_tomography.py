d332920add089210beaf85499fe2dc8f3992f534
qubearfarmer <nbaolong89@gmail.com>
2019-12-22 10:45:27 -0500

    update script

--- Coupled_fluxonium_scripts/Augustus XVIII/Augustus_XVIII_twoQubit_tomography.py
+++ Coupled_fluxonium_scripts/Augustus XVIII/Augustus_XVIII_twoQubit_tomography.py
@@ -106,99 +106,110 @@ matrix_histogram_complex(rho_reconstructed)
 # rho_ideal = ket2dm(tensor(rx(phi=np.pi/2)*basis(2,0),rx(phi=np.pi/2)*basis(2,0)))
 # rho_ideal = ket2dm(tensor(basis(2,1),basis(2,1)))
 # rho_ideal = ket2dm(tensor(rx(phi=np.pi/2), qeye(2))*csign()*tensor(rx(phi=np.pi/2)*basis(2,0),rx(phi=np.pi/2)*basis(2,0)))
-rho_ideal = ket2dm(tensor(rx(phi=np.pi/2), qeye(2))*cphase(theta=np.pi)*tensor(rx(phi=np.pi/2)*basis(2,0),rx(phi=np.pi/2)*basis(2,0)))
-rho_ideal = ket2dm(tensor(rx(phi=np.pi/2), qeye(2))*tensor(rx(phi=np.pi), qeye(2))*cphase(theta=np.pi)*tensor(rx(phi=np.pi/2)*basis(2,0),rx(phi=np.pi/2)*basis(2,0)))
-plt.title((fidelity(rho_reconstructed, rho_ideal)))
-matrix_histogram_complex(rho_ideal)
-w1 = 0.25*(II+XX-YY-ZZ)
-w2 = 0.25*(II-XX+YY-ZZ)
-w3 = 0.25*(II-XX-YY+ZZ)
-w4 = 0.25*(II+XX+YY+ZZ)
-c1 = XX+XZ-ZX+ZZ
-c2 = XX-XZ+ZX+ZZ
-c3 = YY+YZ-ZY+ZZ
-c4 = YY-YZ+ZY+ZZ
-print ((expect(w1,rho_reconstructed)))
-print ((expect(w2,rho_reconstructed)))
-print ((expect(w3,rho_reconstructed)))
-print ((expect(w4,rho_reconstructed)))
+# wA = 2*np.pi*72.4e6
+# wB = 2*np.pi*136.3e6
+# t=2*0.8e-6
+# initial_state = tensor(basis(2,0), basis(2,0))
+# zA = tensor(rz(phi=wA*t),qeye(2))
+# zB = tensor(qeye(2), rz(phi=wB*t))
+# X2p_AB = tensor(rx(phi=np.pi/2), rx(phi=np.pi/2))
+# X_A = tensor(rx(phi=np.pi), qeye(2))
+# X2p_A = tensor(rx(phi=np.pi/2), qeye(2))
+# rho_ideal = ket2dm(X2p_A*X_A*csign()*X2p_AB*initial_state)
+# plt.title((fidelity(rho_reconstructed, rho_ideal)))
+# matrix_histogram_complex(rho_ideal)
+# rho_ideal = ket2dm(X2p_A*X_A*zA*zB*csign()*X2p_AB*initial_state)
+# matrix_histogram_complex(rho_ideal)
+# w1 = 0.25*(II+XX-YY-ZZ)
+# w2 = 0.25*(II-XX+YY-ZZ)
+# w3 = 0.25*(II-XX-YY+ZZ)
+# w4 = 0.25*(II+XX+YY+ZZ)
+# c1 = XX+XZ-ZX+ZZ
+# c2 = XX-XZ+ZX+ZZ
+# c3 = YY+YZ-ZY+ZZ
+# c4 = YY-YZ+ZY+ZZ
+# print ((expect(w1,rho_reconstructed)))
+# print ((expect(w2,rho_reconstructed)))
+# print ((expect(w3,rho_reconstructed)))
+# print ((expect(w4,rho_reconstructed)))
 # print (abs(expect(c1,rho_reconstructed)))
 # print (abs(expect(c2,rho_reconstructed)))
 # print (abs(expect(c3,rho_reconstructed)))
 # print (abs(expect(c4,rho_reconstructed)))
-######################################################################
-# gate_sequence = np.array(['I-I','Xp-I','I-Xp',\
-#                           'X2p-I','X2p-X2p','X2p-Y2p','X2p-Xp',\
-#                           'Y2p-I','Y2p-X2p','Y2p-Y2p','Y2p-Xp',\
-#                           'I-X2p','Xp-X2p','I-Y2p','Xp-Y2p'])
-# sI = np.array([[1,0],[0,1]])
-# sX = np.array([[0,1],[1,0]])
-# sY = np.array([[0,-1j],[1j,0]])
-# sZ = np.array([[1,0],[0,-1]])
-#
-# II = np.kron(sI,sI)
-# IX = np.kron(sI,sX)
-# IY = np.kron(sI,sY)
-# IZ = np.kron(sI,sZ)
-# XI = np.kron(sX,sI)
-# XX = np.kron(sX,sX)
-# XY = np.kron(sX,sY)
-# XZ = np.kron(sX,sZ)
-# YI = np.kron(sY,sI)
-# YX = np.kron(sY,sX)
-# YY = np.kron(sY,sY)
-# YZ = np.kron(sY,sZ)
-# ZI = np.kron(sZ,sI)
-# ZX = np.kron(sZ,sX)
-# ZY = np.kron(sZ,sY)
-# ZZ = np.kron(sZ,sZ)
-#
-# M = np.zeros((len(gate_sequence), 4, 4), dtype = complex)
-# M[0,:,:] = betaII*II + betaZI*ZI + betaIZ*IZ + betaZZ*ZZ
-# M[1,:,:] = betaII*II - betaZI*ZI + betaIZ*IZ - betaZZ*ZZ
-# M[2,:,:] = betaII*II + betaZI*ZI - betaIZ*IZ - betaZZ*ZZ
-#
-# M[3,:,:] = betaII*II + betaZI*YI + betaIZ*IZ + betaZZ*YZ
-# M[4,:,:] = betaII*II + betaZI*YI + betaIZ*IY + betaZZ*YY
-# M[5,:,:] = betaII*II + betaZI*YI - betaIZ*IX - betaZZ*YX
-# M[6,:,:] = betaII*II + betaZI*YI - betaIZ*IZ - betaZZ*YZ
-#
-# M[7,:,:] = betaII*II - betaZI*XI + betaIZ*IZ - betaZZ*XZ
-# M[8,:,:] = betaII*II - betaZI*XI + betaIZ*IY - betaZZ*XY
-# M[9,:,:] = betaII*II - betaZI*XI - betaIZ*IX + betaZZ*XX
-# M[10,:,:] = betaII*II - betaZI*XI - betaIZ*IZ + betaZZ*XZ
-#
-# M[11,:,:] = betaII*II + betaZI*ZI + betaIZ*IY + betaZZ*ZY
-# M[12,:,:] = betaII*II - betaZI*ZI + betaIZ*IY - betaZZ*ZY
-# M[13,:,:] = betaII*II + betaZI*ZI - betaIZ*IX - betaZZ*ZX
-# M[14,:,:] = betaII*II - betaZI*ZI - betaIZ*IX + betaZZ*ZX
-#
-# def density_matrix(t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15):
-#     t16 = 1 - t1 -t2 - t3
-#     tau = np.array([[t1, t4+1j*t5, t6+1j*t7, t8+1j*t9], [0, t2, t10+1j*t11, t12+1j*t13], [0, 0, t3, t14+1j*t15], [0,0,0,t16]])
-#     rho = np.conj(tau.transpose())*tau / np.trace(np.conj(tau.transpose())*tau)
-#     return rho
-#
-# def likelihood(x):
-#     dist = 0
-#     for idx in range(len(gate_sequence)):
-#         dist = dist + abs((m[idx] - np.trace(M[idx, :, :].dot(density_matrix(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10], x[11],x[12],x[13],x[14])))))**2
-#     return dist
-# guess = np.zeros(15)
-# guess[-1] = 1
-# res = minimize(likelihood, guess, method='nelder-mead')
-# t = res.x
-# rho_reconstructed_mle = density_matrix(*t)
-# matrix_histogram_complex(rho_reconstructed_mle)
-########################################################################
-# rho_ideal = ket2dm(tensor(basis(2,0),basis(2,0)))
-# rho_ideal = ket2dm(tensor(rx(phi=np.pi/2)*basis(2,0),rx(phi=np.pi/2)*basis(2,0)))
-# rho_ideal = ket2dm(tensor(basis(2,1),basis(2,1)))
-# rho_ideal = ket2dm(tensor(rx(phi=np.pi/2), qeye(2))*csign()*tensor(rx(phi=np.pi/2)*basis(2,0),rx(phi=np.pi/2)*basis(2,0)))
+#####################################################################
+#Maximum likelihood
+gate_sequence = np.array(['I-I','Xp-I','I-Xp',\
+                          'X2p-I','X2p-X2p','X2p-Y2p','X2p-Xp',\
+                          'Y2p-I','Y2p-X2p','Y2p-Y2p','Y2p-Xp',\
+                          'I-X2p','Xp-X2p','I-Y2p','Xp-Y2p'])
+sI = np.array([[1,0],[0,1]])
+sX = np.array([[0,1],[1,0]])
+sY = np.array([[0,-1j],[1j,0]])
+sZ = np.array([[1,0],[0,-1]])
 
-# print (fidelity(rho_reconstructed, rho_ideal))
-# print (avgXX-avgXZ+avgZX+avgZZ)
-# rho_ideal = ket2dm(bell_state('00'))
-# matrix_histogram_complex(rho_ideal)
-# print (expect(XX,rho_ideal) - expect(XZ,rho_ideal) + expect(ZX,rho_ideal) + expect(ZZ,rho_ideal))
+II = np.kron(sI,sI)
+IX = np.kron(sI,sX)
+IY = np.kron(sI,sY)
+IZ = np.kron(sI,sZ)
+XI = np.kron(sX,sI)
+XX = np.kron(sX,sX)
+XY = np.kron(sX,sY)
+XZ = np.kron(sX,sZ)
+YI = np.kron(sY,sI)
+YX = np.kron(sY,sX)
+YY = np.kron(sY,sY)
+YZ = np.kron(sY,sZ)
+ZI = np.kron(sZ,sI)
+ZX = np.kron(sZ,sX)
+ZY = np.kron(sZ,sY)
+ZZ = np.kron(sZ,sZ)
+
+M = np.zeros((len(gate_sequence), 4, 4), dtype = complex)
+M[0,:,:] = betaII*II + betaZI*ZI + betaIZ*IZ + betaZZ*ZZ
+M[1,:,:] = betaII*II - betaZI*ZI + betaIZ*IZ - betaZZ*ZZ
+M[2,:,:] = betaII*II + betaZI*ZI - betaIZ*IZ - betaZZ*ZZ
+
+M[3,:,:] = betaII*II + betaZI*YI + betaIZ*IZ + betaZZ*YZ
+M[4,:,:] = betaII*II + betaZI*YI + betaIZ*IY + betaZZ*YY
+M[5,:,:] = betaII*II + betaZI*YI - betaIZ*IX - betaZZ*YX
+M[6,:,:] = betaII*II + betaZI*YI - betaIZ*IZ - betaZZ*YZ
+
+M[7,:,:] = betaII*II - betaZI*XI + betaIZ*IZ - betaZZ*XZ
+M[8,:,:] = betaII*II - betaZI*XI + betaIZ*IY - betaZZ*XY
+M[9,:,:] = betaII*II - betaZI*XI - betaIZ*IX + betaZZ*XX
+M[10,:,:] = betaII*II - betaZI*XI - betaIZ*IZ + betaZZ*XZ
+
+M[11,:,:] = betaII*II + betaZI*ZI + betaIZ*IY + betaZZ*ZY
+M[12,:,:] = betaII*II - betaZI*ZI + betaIZ*IY - betaZZ*ZY
+M[13,:,:] = betaII*II + betaZI*ZI - betaIZ*IX - betaZZ*ZX
+M[14,:,:] = betaII*II - betaZI*ZI - betaIZ*IX + betaZZ*ZX
+
+def density_matrix(t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15):
+    t16 = 1 - t1 -t2 - t3
+    tau = np.array([[t1, t4+1j*t5, t6+1j*t7, t8+1j*t9], [0, t2, t10+1j*t11, t12+1j*t13], [0, 0, t3, t14+1j*t15], [0,0,0,t16]])
+    rho = np.conj(tau.transpose()).dot(tau)
+    rho = rho / np.trace(rho)
+    return rho
+
+def likelihood(x):
+    dist = 0
+    for idx in range(len(gate_sequence)):
+        dist = dist + abs((m[idx] - np.trace(M[idx, :, :].dot(density_matrix(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10], x[11],x[12],x[13],x[14])))))**2
+    return dist
+def fidelity(rho,rho_ideal):
+    return abs(np.trace(np.sqrt(np.sqrt(rho_ideal).dot(rho).dot(np.sqrt(rho_ideal)))))
+
+guess = np.zeros(15)
+guess[0]=1
+guess[7]=1
+res = minimize(likelihood, guess, method='powell',tol=1.e-10,
+            options={'maxiter': 10000})
+t = res.x
+rho_reconstructed_mle = Qobj(density_matrix(*t))
+rho_ideal = density_matrix(*guess)
+
+matrix_histogram_complex(rho_reconstructed_mle)
+plt.title(fidelity(rho_reconstructed_mle, rho_ideal))
+matrix_histogram_complex(rho_ideal)
+#######################################################################
 plt.show()
